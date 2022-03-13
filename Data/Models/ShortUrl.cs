0e9343824e152358fa1ab843c8eaa6c59726b5fb
pioner951 <Romashka951@gmail.com>
2022-03-13 13:44:35 +0500

    Создание моделей

---
+++ Data/Models/ShortUrl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShotenerBlazor.Data.Models
+{
+    public class ShortUrl
+    {
+        public int Id { get; set; }
+        public Uri OriginalUrl { get; set; }
+        public Uri Value { get; set; }
+        public string Token { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int Clicks { get; set; }
+
+    }
+}
---
+++ Data/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShotenerBlazor.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public ICollection<ShortUrl> shortUrls { get; set; }
+    }
+}
