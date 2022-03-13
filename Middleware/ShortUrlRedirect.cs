c07279a64f7e87ae1924321ec903f83f4b01edcf
pioner951 <Romashka951@gmail.com>
2022-03-13 14:30:05 +0500

    Настройка сокращения ссылок

---
+++ Middleware/ShortUrlRedirect.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ShotenerBlazor.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ShortUrlRedirect
+    {
+        private readonly RequestDelegate _next;
+
+        public ShortUrlRedirect(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ShortUrlRedirectExtensions
+    {
+        public static IApplicationBuilder UseShortUrlRedirect(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ShortUrlRedirect>();
+        }
+    }
+}
